8c5cdb7a09fdabc66520064982e3d71c82cc51ea
pindellk <pindellk@protonmail.com>
2020-11-20 22:48:21 -0500

    Add database creation and insert 3 values

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Santa Fe Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chick Pea Burger');
